9e34cf25dc6731006a27237b2c795d0b2a9c1986
Joel Van Eenwyk <joel.vaneenwyk@gmail.com>
2024-05-09 19:12:48 -0700

    add prettier

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,5 +1,6 @@
 import globals from "globals";
 import pluginJs from "@eslint/js";
+import eslintConfigPrettier from "eslint-config-prettier";
 
 /** @type {import("eslint-config")} */
 export default [
@@ -16,5 +17,11 @@ export default [
       }
     }
   },
-  pluginJs.configs.recommended
+  pluginJs.configs.recommended,
+  {
+    ignores: [
+      "dist/main.js"
+    ]
+  },
+  eslintConfigPrettier
 ];
